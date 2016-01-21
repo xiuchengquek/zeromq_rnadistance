c53d174a3afc2d2456759a615f454e0ca749db8d
xiuchengquek <biobirdman@gmail.com>
2016-01-21 17:56:42 +1100

    change methods

--- ventilator_match_score.py
+++ ventilator_match_score.py
@@ -16,7 +16,7 @@ if __name__ == '__main__' :
 
     wd = '/share/ClusterScratch/xiuque/martinSmith/data/completed/split_detailed'
     structure_dir = '/share/ClusterScratch/xiuque/martinSmith/data/rnadistance_results'
-    out_dir = '/share/ClusterScratch/xiuque/marthinSmith/data/outdir/'
+    out_dir = '/share/ClusterScratch/xiuque/martinSmith/data/outdir/'
 
     sample_file_list = os.listdir(wd)
 
@@ -39,8 +39,10 @@ if __name__ == '__main__' :
         structure_score_file = os.path.join(structure_dir, structure_file_name)
 
         sample_file = os.path.join(wd, values)
-        assert os.path.exists(structure_score_file)
-        assert os.path.exists(sample_file)
+        assert os.path.exists(structure_score_file) , "structure file missing %s" % structure_score_file
+        assert os.path.exists(sample_file) , "sample_file file missing %s" % sample_file
+        assert os.path.exists(out_dir) , "out_dir file missing %s" % out_dir
+
 
         data_package = { 'alignment_pair' : values,
           'sample_file' : values ,
